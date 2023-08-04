0c1dbf60dbb74599375a4c74aa61f0b85ed32bd7
unkokaeru <110934475+unkokaeru@users.noreply.github.com>
2023-08-04 11:00:02 +0000

    Added main() docstring and linted code

--- SMMO REQUEST ANY.py
+++ SMMO REQUEST ANY.py
@@ -13,25 +13,25 @@ API_KEY = (
 )
 
 # IDs, if not known, run and select option 2
-player_id = 1019244
-guild_id = 2746
-item_id = 1
+PLAYER_ID = 1019244
+GUILD_ID = 2746
+ITEM_ID = 1
 
 # API Options
 options = {
-    "Player Information": f"https://api.simple-mmo.com/v1/player/info/{player_id}",
+    "Player Information": f"https://api.simple-mmo.com/v1/player/info/{PLAYER_ID}",
     "Your Information": "https://api.simple-mmo.com/v1/player/me",
-    "Player Equipped Items": f"https://api.simple-mmo.com/v1/player/equipment/{player_id}",
-    "Player Skills": f"https://api.simple-mmo.com/v1/player/skills/{player_id}",
+    "Player Equipped Items": f"https://api.simple-mmo.com/v1/player/equipment/{PLAYER_ID}",
+    "Player Skills": f"https://api.simple-mmo.com/v1/player/skills/{PLAYER_ID}",
     "Diamond Market": "https://api.simple-mmo.com/v1/diamond-market",
     "Orphanage": "https://api.simple-mmo.com/v1/orphanage",
     "World Bosses": "https://api.simple-mmo.com/v1/worldboss/all",
-    "Item Information": f"https://api.simple-mmo.com/v1/item/info/{item_id}",
+    "Item Information": f"https://api.simple-mmo.com/v1/item/info/{ITEM_ID}",
     "List of Guilds": "https://api.simple-mmo.com/v1/guilds/all",
-    "Guild Information": f"https://api.simple-mmo.com/v1/guilds/info/{guild_id}",
-    "Guild Members": f"https://api.simple-mmo.com/v1/guilds/members/{guild_id}",
-    "Guild Member Contribution": f"https://api.simple-mmo.com/v1/guilds/members/{guild_id}/contribution/{player_id}",
-    "Guild Wars": f"https://api.simple-mmo.com/v1/guilds/wars/{guild_id}/4",
+    "Guild Information": f"https://api.simple-mmo.com/v1/guilds/info/{GUILD_ID}",
+    "Guild Members": f"https://api.simple-mmo.com/v1/guilds/members/{GUILD_ID}",
+    "Guild Member Contribution": f"https://api.simple-mmo.com/v1/guilds/members/{GUILD_ID}/contribution/{PLAYER_ID}",
+    "Guild Wars": f"https://api.simple-mmo.com/v1/guilds/wars/{GUILD_ID}/4",
     "Guild Seasons": "https://api.simple-mmo.com/v1/guilds/seasons",
     "Guild Season 1 Leaderboards": "https://api.simple-mmo.com/v1/guilds/seasons/1",
     "Guild Season 2 Leaderboards": "https://api.simple-mmo.com/v1/guilds/seasons/2",
@@ -48,6 +48,7 @@ def fetch_data(choice: int) -> str:
     request = requests.post(
         f"{list(options.items())[choice - 1][1]}",
         data={"api_key": API_KEY},
+        timeout=1
     )
     if request.status_code == 200:
         formatted_request = json.dumps(json.loads(request.text), indent=2)
@@ -67,8 +68,15 @@ def fetch_data(choice: int) -> str:
 
 
 def main() -> None:
+    """
+    The main() function offers a menu of options to the user to choose from.
+    It then passes the user's choice to the fetch_data() and collect_data() functions.
+    It also includes validation for the user input and recursively calls itself
+    if an invalid option is selected.
+    """
+
     # Print menu options
-    for item, url in options.items():
+    for item in options.items():
         print(f"{list(options.keys()).index(item) + 1}. {item.title()}")
 
     # Take user option choice
